1685866da9fca32f8d09897478563dce55e573d2
Yuka Mullen <quiwah@gmail.com>
2018-07-07 19:26:12 -0700

    display the cats' names

--- js/catClicker.js
+++ js/catClicker.js
@@ -2,19 +2,25 @@ document.addEventListener("DOMContentLoaded", function() {
     let cat1 = document.getElementById('cougar');
     let cat2 = document.getElementById('cheetah');
     
-    const catName1 = 'Cougres';
-    const catName2 = 'Flash';
+    const cat1Name = 'Cougres';
+    const cat2Name = 'Flash';
+
+    const count1 = document.getElementById('count-cougar');
+    const count2 = document.getElementById('count-cheetah');
+
+    //Display cats' names
+    count1.insertAdjacentHTML('beforebegin', cat1Name + ' ');
+    count2.insertAdjacentHTML('beforebegin', cat2Name + ' ');
+    
 
     let i = 0;
     let c = 0;
     cat1.addEventListener('click', function() {
             i++;
-            const count1 = document.getElementById('count-cougar');
             count1.innerHTML = i;
     }, false);
     cat2.addEventListener('click', function() {
         c++;
-        const count2 = document.getElementById('count-cheetah');
         count2.innerHTML = c;
 }, false);
 });
