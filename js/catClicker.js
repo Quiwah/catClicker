681a1ac5d7775821aef471a086fe78914d5af538
Yuka Mullen <quiwah@gmail.com>
2018-07-07 17:27:14 -0700

    Add another cat

--- js/catClicker.js
+++ js/catClicker.js
@@ -1,9 +1,20 @@
 document.addEventListener("DOMContentLoaded", function() {
-    let cat = document.getElementById('cougar');
+    let cat1 = document.getElementById('cougar');
+    let cat2 = document.getElementById('cheetah');
+    
+    const catName1 = 'Cougres';
+    const catName2 = 'Flash';
+
     let i = 0;
-    cat.addEventListener('click', function() {
+    let c = 0;
+    cat1.addEventListener('click', function() {
             i++;
-            const count = document.getElementById('count');
-            count.innerHTML = i;
+            const count1 = document.getElementById('count-cougar');
+            count1.innerHTML = i;
     }, false);
+    cat2.addEventListener('click', function() {
+        c++;
+        const count2 = document.getElementById('count-cheetah');
+        count2.innerHTML = c;
+}, false);
 });
